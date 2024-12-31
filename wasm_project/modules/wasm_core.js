56f485407ecffb97ccbaf6e0a32bde9f8ed386e1
Benn Herrera <bennherrera@pm.me>
2024-12-31 02:01:45 -0800

    minor refactor.

--- wasm_project/modules/wasm_core.js
+++ wasm_project/modules/wasm_core.js
@@ -5,54 +5,27 @@ import createBngWasmModule from "/modules/bng.js"
 var wasm_module = null
 var engine_handle = null
 
-function solve(puzzle) {
-  return wasm_module.bng_engine_solve(engine_handle, puzzle)
-}
-
-function setup(word_list) {
-  return wasm_module.bng_engine_setup(engine_handle, word_list)
-}
-
-
-function destroy() {
+function wasm_core_init(word_list, on_ready) {
   if (engine_handle != null) {
     wasm_module.bng_engine_destroy(engine_handle)
     engine_handle = null
   }
-}
-
-function create() {
-  engine_handle = wasm_module.bng_engine_create()
-}
-
-// use this to skip 16 bit to 8 bit strings
-// https://cs-263-emscripten.readthedocs.io/en/latest/files.html
-// FS.createDataFile("/", "file.txt", "File contents here", true, true);
-
-function wasm_core_init(word_list, on_ready) {
-  if (wasm_module != null) {
-    assert(false, "wasm_core_init() already called.")
-    return
-  }
   createBngWasmModule().then((inst) => {
-    assert(inst != null, "module initialization failed!")
-    destroy()
     wasm_module = inst
-    create()
-    let err_msg = setup(word_list)
-    if (err_msg) {
-      alert(err_msg)
-      return
-    }
-    if (on_ready != null) {
+    engine_handle = wasm_module.bng_engine_create()
+    let err_msg = wasm_module.bng_engine_setup(engine_handle, word_list)
+    if (!err_msg) {
       on_ready()
+    }
+    else {
+      assert(false, err_msg)
     }
   })
 }
 
 function wasm_core_solve(puzzle) {
-  assert(wasm_module != null, "wasm_core_init() not completed.")
-  return solve(puzzle)
+  assert(engine_handle != null, "wasm_core_init() not completed.")
+  return wasm_module.bng_engine_solve(engine_handle, puzzle)
 }
 
 const WasmCore = { init: wasm_core_init, solve: wasm_core_solve }
